1b506a40ac91e020e29f09e1c6388ac3ce70edb3
Victor Lin <linvictorw@gmail.com>
2022-03-29 11:24:07 +0800

    Add if statements

--- trueFalse.go
+++ trueFalse.go
@@ -8,6 +8,15 @@ func compare(value int) string {
 	secretValue := 88
 
 	//Insert your code from here
+	if value == secretValue {
+		fmt.Println("Well Done! Your guess is correct")
+	}
+	if value < secretValue {
+		fmt.Println("Too low, try again next time!")
+	}
+	if value > secretValue {
+		fmt.Println("Too high, try again next time!")
+	}
 
 	//do not remove this line
 	return resultMessge
